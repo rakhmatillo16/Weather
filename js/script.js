b1d51ea2aee651515b8aa73930a6a94113e6f628
rakhmatillo16 <rahmatillorasulov2000@gmail.com>
2022-02-04 16:06:40 +0500

    front styled

--- js/script.js
+++ js/script.js
@@ -6,20 +6,27 @@ let input = document.querySelector(".input");
 
 const renderWeather = function (data) {
   const html = `
-    <h2></h2>
-    <p>temperature: ${data.temperature}</p>
-    <p>wind: ${data.wind}</p>
-    <p>description:  ${data.description}</p>
-    <ul>
-        <li>Erta:
+  <h2 class = "continent">${input.value}</h2>
+  <div class = "today-weather">
+      <h2>Today:</h2>
+      <p>temperature: ${data.temperature}</p>
+      <p>wind: ${data.wind}</p>
+      <p>description:  ${data.description}</p>
+    </div>
+
+    <ul class = "weather-list">
+        <li class = "weather-item">
+          <h4>Tomorrow:</h4>
           <p>temperature: ${data.forecast[0].temperature}</p>
           <p>wind: ${data.forecast[0].wind}</p>
           <p>description:  ${data.forecast[0].description}</p></li>
-        <li>Indin:
+        <li class = "weather-item">
+          <h4>The next day:</h4>
           <p>temperature: ${data.forecast[1].temperature}</p>
           <p>wind: ${data.forecast[1].wind}</p>
           <p>description:  ${data.forecast[1].description}</p></li></li>
-        <li>Va so'nggi kun:
+        <li class = "weather-item">
+          <h4>Few days later:</h4>
           <p>temperature: ${data.forecast[2].temperature}</p>
           <p>wind: ${data.forecast[2].wind}</p>
           <p>description:  ${data.forecast[2].description}</p></li></li>
